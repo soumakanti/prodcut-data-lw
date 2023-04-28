613c5bb9ffc4475f10141604368e6c46d15d4f3f
Souma Kanti <soumatkd@gmail.com>
2023-04-28 12:36:51 +0530

    commit

--- all-product-data/src/main/java/com/xoloti/product/allproductdata/AllProductDataApplication.java
+++ all-product-data/src/main/java/com/xoloti/product/allproductdata/AllProductDataApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AllProductDataApplication {
- 
+  
 	public static void main(String[] args) {
 		SpringApplication.run(AllProductDataApplication.class, args);
 	}
